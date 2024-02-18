10845a752c0e5c8ab7ac7cd1e1fa7f6c460c65fb
1michael17 <94114865+1michael17@users.noreply.github.com>
2024-02-18 17:54:36 +0100

    Added function for Computer random choice

--- ropasci-script.js
+++ ropasci-script.js
@@ -4,31 +4,39 @@
 
     // for loop to count to 5
 
-for (let i = 0; i < 5; i++) {
-    // computer choice and validation of player's choice
+for (let i = 4; i >= 0; i--) {
+    // computer and player choices
     const computerChoice = ['rock', 'paper', 'scissors'];
     const playerChoice = String(prompt(`Enter: 'Rock', 'Paper', 'Scissors'`).toLocaleLowerCase());
+    //  validation of player choice
+//     if (playerChoice === 'rock'|| playerChoice ==='paper' || playerChoice ==='scissor'){
+// }
+
+    //function for Computer Random choice
+    const chooser = (arg) =>{
+        let randomArgIndex = Math.floor(Math.random()*computerChoice.length);
+        return arg[randomArgIndex];
+    }
+    const computerValue = chooser(computerChoice);
+    console.log(computerValue);
+
+    //Validator for draw
+    if( computerValue === playerChoice ) {
+        console.log('DRAW');
+        i++;
+        console.log(i);
+    }
+    else console.log('Not DRAW');
+
+    console.log(`Player's Choice === ${playerChoice}, Computer's Choice === ${computerValue}
+        Chances remaining = ${i}
+    `);
+    
     
 
+
+
+
+
 }
-
-
-
-    // fn() for computer random choice
-    // scorer fn() for the winner
-
-    // const computerChoice = ['rock', 'paper', 'scissors'];
-    // const playerChoice = String(prompt(`Enter: 'Rock', 'Paper', 'Scissors'`).toLocaleLowerCase());
-
-    // console.log(playerChoice);
-    // let dindex = Number(Math.floor(Math.random()*computerChoice.length));
-    // let  comC = computerChoice[dindex];
-
-    // console.log(dindex,comC);
-
-    // if (comC === playerChoice) {
-    //     console.log('hurray');
-    // }
-    // tally--
-
 
