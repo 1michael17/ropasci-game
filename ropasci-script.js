dcb83ea700dcd88d974ebdf67e0b181f5048b0de
1michael17 <94114865+1michael17@users.noreply.github.com>
2024-02-18 14:26:47 +0100

    Added const for computer and player choice

--- ropasci-script.js
+++ ropasci-script.js
@@ -2,30 +2,33 @@
 
     // Add addEventlistener to call play function
 
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        
-    }
     // for loop to count to 5
 
-
-
+for (let i = 0; i < 5; i++) {
     // computer choice and validation of player's choice
+    const computerChoice = ['rock', 'paper', 'scissors'];
+    const playerChoice = String(prompt(`Enter: 'Rock', 'Paper', 'Scissors'`).toLocaleLowerCase());
+    
+
+}
+
+
+
     // fn() for computer random choice
     // scorer fn() for the winner
 
-    const computerChoice = ['rock', 'paper', 'scissors'];
-    const playerChoice = String(prompt(`Enter: 'Rock', 'Paper', 'Scissors'`).toLocaleLowerCase());
+    // const computerChoice = ['rock', 'paper', 'scissors'];
+    // const playerChoice = String(prompt(`Enter: 'Rock', 'Paper', 'Scissors'`).toLocaleLowerCase());
 
-    console.log(playerChoice);
-    let dindex = Number(Math.floor(Math.random()*computerChoice.length));
-    let  comC = computerChoice[dindex];
+    // console.log(playerChoice);
+    // let dindex = Number(Math.floor(Math.random()*computerChoice.length));
+    // let  comC = computerChoice[dindex];
 
-    console.log(dindex,comC);
+    // console.log(dindex,comC);
 
-    if (comC === playerChoice) {
-        console.log('hurray');
-    }
-    tally--
+    // if (comC === playerChoice) {
+    //     console.log('hurray');
+    // }
+    // tally--
 
 
