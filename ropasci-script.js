08060f8afd85ce1317a605ee564a66a3197e4942
1michael17 <94114865+1michael17@users.noreply.github.com>
2024-02-19 09:42:53 +0100

    Added eventListener to button to run play function

--- ropasci-script.js
+++ ropasci-script.js
@@ -1,49 +1,54 @@
 
 // Add addEventlistener to call play function
+const button = document.querySelector('.play');
+button.addEventListener('click', play)
+function play(){
 
-    // for loop to count to 5
+        // for loop to count to 5
 
-    const chooser = (arg) =>{
-        let randomArgIndex = Math.floor(Math.random()*3);
-        return arg[randomArgIndex];
-    }
-    let pScore = 0; let cScore = 0;
-
-for (let i = 4; i >= 0; i--) {
-    // computer and player choices
-    const computerChoice = ['rock', 'paper', 'scissors'];
-    const playerChoice = String(prompt(`Enter: 'Rock', 'Paper', 'Scissors'`).toLocaleLowerCase());
-
-    //  validation of player choice
-    if (playerChoice === 'rock'|| playerChoice ==='paper' || playerChoice ==='scissor'){
-        //function for Computer Random choice
-        const computerValue = chooser(computerChoice);
-      
-        //Validator for draw
-        if( computerValue === playerChoice ) {
-            console.log('DRAW');
-            i++;
-        }else {
-            console.log('Not DRAW');
-
-            // Score
-            if( computerValue === 'rock' && playerChoice === 'paper'|| computerValue === 'paper' && playerChoice === 'scissor' || computerValue === 'scissor' && playerChoice === 'rock'){
-                pScore++;
-            }
-            else{
-                cScore++;
-            }
+        const chooser = (arg) =>{
+            let randomArgIndex = Math.floor(Math.random()*3);
+            return arg[randomArgIndex];
         }
+        let pScore = 0; let cScore = 0;
 
-        console.log(`
-        Player's Choice === ${playerChoice} 
-        Computer's Choice === ${computerValue}
-        Chances remaining = ${i}
-        Score:: Player = ${pScore}, Computer Score = ${cScore};
-        `);
+    for (let i = 4; i >= 0; i--) {
+        // computer and player choices
+        const computerChoice = ['rock', 'paper', 'scissors'];
+        const playerChoice = String(prompt(`Enter: 'Rock', 'Paper', 'Scissors'`).toLocaleLowerCase());
 
-        if (pScore === 3 || cScore === 3) {
-            i = 0;
+        //  validation of player choice
+        if (playerChoice === 'rock'|| playerChoice ==='paper' || playerChoice ==='scissor'){
+            //function for Computer Random choice
+            const computerValue = chooser(computerChoice);
+        
+            //Validator for draw
+            if( computerValue === playerChoice ) {
+                console.log('DRAW');
+                i++;
+            }else {
+                console.log('Not DRAW');
+
+                // Score
+                if( computerValue === 'rock' && playerChoice === 'paper'|| computerValue === 'paper' && playerChoice === 'scissor' || computerValue === 'scissor' && playerChoice === 'rock'){
+                    pScore++;
+                }
+                else{
+                    cScore++;
+                }
+            }
+
+            console.log(`
+            Player's Choice === ${playerChoice} 
+            Computer's Choice === ${computerValue}
+            Chances remaining = ${i}
+            Score:: Player = ${pScore}, Computer Score = ${cScore};
+            `);
+
+            // Validator to 3 as max score
+            if (pScore === 3 || cScore === 3) {
+                i = 0;
+            }
         }
     }
 }
