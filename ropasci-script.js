93386a21b67dabef42f4832c620bd37174292296
1michael17 <94114865+1michael17@users.noreply.github.com>
2024-05-29 19:17:27 +0100

    Added default values

--- ropasci-script.js
+++ ropasci-script.js
@@ -1,3 +1,31 @@
+const dBody = document.querySelector('.body');
+const dResult = document.querySelector('.toPlay');
+const toStart = document.querySelector('#start');
+const toReset = document.querySelector('.playReset');
+const option = document.querySelector('.option');
+const choose = document.querySelectorAll('.choose');
+const scoreScreen = document.querySelector('.score');
+const urPoint = document.querySelector('.urPoint');
+const UrChoice = document.querySelector('.UrChoice');
+const cmPoint = document.querySelector('.cmPoint');
+const cmChoice = document.querySelector('.cmChoice');
+const drwPoint = document.querySelector('.drwPoint');
+const h2Det = document.querySelector('.a');
+const hw2Play = document.querySelector('#toPlay');
+const detailUL = document.querySelector('#detail');
+//-- End of Query Selector*/
+scoreScreen.style.display = 'none';
+dResult.style.display = 'none';
+toReset.style.display = 'none';
+
+// -- END OF Default Hidden 
+let pScore = 0, cScore = 0, drwScore = 0; let playerClick;
+
+choose.forEach(e=>{
+    return e.setAttribute('disabled','disabled');
+});
+
+//-- END OF Default Values
 
 function toFirstUppercase(str){
     //for converting first letter to uppercase
